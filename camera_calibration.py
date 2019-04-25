99ce3603947342b7f8041187c6da37aa26cac9d4
giogia <Giovannitommasi95@gmail.com>
2019-04-25 10:21:18 +0800

    camera calibration and added comments

--- camera_calibration.py
+++ camera_calibration.py
@@ -3,57 +3,54 @@ import cv2
 import glob
 
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+def remove_radial_distortion(image):
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7, 3), np.float32)
-objp[:, :2] = np.mgrid[0:7,0:6].T.reshape(-1, 2)
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# Arrays to store object points and image points from all the images.
-object_points = []  # 3d point in real world space
-image_points = []  # 2d points in image plane.
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    object_p = np.zeros((6 * 7, 3), np.float32)
+    object_p[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
 
-images = glob.glob('*.jpg')
+    # Arrays to store object points and image points from all the images.
+    object_points = []  # 3d point in real world space
+    image_points = []  # 2d points in image plane.
 
-for image in images:
-    img = cv2.imread(image)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    calibration_images = glob.glob('calibration/*.jpg')
 
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+    for calibration_image in calibration_images:
 
-    # If found, add object points, image points (after refining them)
-    if ret:
-        object_points.append(objp)
+        img = cv2.imread(calibration_image)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-        image_points.append(corners2)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
 
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7, 6), corners2,ret)
-        cv2.imshow('img', img)
-        cv2.waitKey(500)
+        # If found, add object points, image points (after refining them)
+        if ret:
+            object_points.append(object_p)
 
-cv2.destroyAllWindows()
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            image_points.append(corners2)
 
-ret, mtx, dist, r_vectors, t_vectors = cv2.calibrateCamera(object_points, image_points, gray.shape[::-1], None, None)
+            # Draw and display the corners
+            # img = cv2.drawChessboardCorners(img, (7, 6), corners2, ret)
+            # cv2.imshow('img', img)
+            # cv2.waitKey(500)
 
-img = cv2.imread('left12.jpg')
-height, width = img.shape[:2]
-new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (width, height), 1, (width, height))
+    cv2.destroyAllWindows()
 
-dst = cv2.undistort(img, mtx, dist, None, new_camera_matrix)
+    ret, mtx, dist, r_vectors, t_vectors = cv2.calibrateCamera(object_points, image_points, gray.shape[::-1], None, None)
 
-# crop the image
-x, y, width, height = roi
-dst = dst[y:y + height, x:x + width]
-cv2.imwrite('calibration_result.png', dst)
+    height, width = image.shape[:2]
 
-map_x, map_y = cv2.initUndistortRectifyMap(mtx, dist, None, new_camera_matrix, (width, height), 5)
-dst = cv2.remap(img, map_x, map_y, cv2.INTER_LINEAR)
+    new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (width, height), 1, (width, height))
 
-# crop the image
-x, y, width, height = roi
-dst = dst[y:y + height, x:x + width]
-cv2.imwrite('calibration_result.png', dst)
+    undistorted_image = cv2.undistort(image, mtx, dist, None, new_camera_matrix)
+
+    # crop the image
+    # x, y, width, height = roi
+    # undistorted_image = undistorted_image[y:y + height, x:x + width]
+    # cv2.imwrite('calibration_result.jpg', undistorted_image)
+
+    return undistorted_image
--- measure_height.py
+++ measure_height.py
@@ -1,21 +1,23 @@
 from draw_lines import *
 from vanishing_line import *
+from camera_calibration import *
 
 PATH = 'image.png'
 OBJECT_HEIGHT = 1
 
 # Read image
 image = cv2.imread(PATH)
+image = remove_radial_distortion(image)
 
 # Get Base object and Person
+base_object = get_lines(image, 1, "One Line for Base Object")
+draw_line(image, base_object, COLOUR_GREEN)
 
-object = get_lines(image, 1, "One Line for Base Object")
-draw_line(image, object, COLOUR_GREEN)
 person = get_lines(image, 1, "One Line for Person")
 draw_line(image, person, COLOUR_BLUE)
+
 # Get lines for plane vanishing line
 vanishing_line = []
-
 for i in range(2):
     horizontal_lines = get_lines(image, 2, "Two plane lines")
     horizontal_vanishing_point = intersection_point(horizontal_lines[0], horizontal_lines[1])
@@ -25,39 +27,43 @@ for i in range(2):
         horizontal_vanishing_point = intersection_point(horizontal_lines[0], horizontal_lines[1])
 
     vanishing_line.append(horizontal_vanishing_point)
-    draw_point(image, horizontal_vanishing_point)
 
 vanishing_line = np.asarray(vanishing_line, dtype=int)
 draw_line(image, vanishing_line, COLOUR_WHITE)
 
-plane_line = np.array([object[0], person[0]], dtype=int)
+# Line between object and person bottoms
+plane_line = np.array([base_object[0], person[0]], dtype=int)
 draw_line(image, plane_line, COLOUR_WHITE)
 
+# Intersection of plane line with vanishing line
 vanishing_line_intersection = intersection_point(plane_line, vanishing_line)
 draw_point(image, vanishing_line_intersection, COLOUR_GREEN)
 
-object_projection = np.array([object[1], vanishing_line_intersection], dtype=int)
+# Project object on person from vanishing line
+object_projection = np.array([base_object[1], vanishing_line_intersection], dtype=int)
 object_height = intersection_point(person, object_projection)
 
-top_line = np.array([object[1], person[1]], dtype=int)
+# Line between object and person tops
+top_line = np.array([base_object[1], person[1]], dtype=int)
 draw_line(image, top_line, COLOUR_WHITE)
 
-parallel_person_top = np.subtract(person[0], np.subtract(object[0], object[1]))
+# Person line parallel to object line
+parallel_person_top = np.subtract(person[0], np.subtract(base_object[0], base_object[1]))
 parallel_person = np.array([person[0], parallel_person_top])
-
 person_height_point = intersection_point(top_line, parallel_person)
 draw_point(image, person_height_point, COLOUR_BLUE)
 person_height_line = np.array([person_height_point, person[0]])
 draw_line(image, person_height_line, COLOUR_BLUE)
 
+# Object projection on parallel person line
 parallel_object_height = intersection_point(parallel_person, object_projection)
 draw_point(image, parallel_object_height, COLOUR_GREEN)
 parallel_object_line = np.array([parallel_object_height, person[0]])
 draw_line(image, parallel_object_line, COLOUR_GREEN)
 
+# Compute ration between the two heights
 person_height = OBJECT_HEIGHT * np.linalg.norm(person[0] - person_height_point) \
                 / np.linalg.norm(person[0] - parallel_object_height)
-
 write_message(image, str(person_height))
 
 cv2.namedWindow("Image", cv2.WINDOW_GUI_NORMAL)
